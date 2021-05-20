a28541fa63e9ed13a068e7d0f58bfedbff0f0715
Reinaldo da Silva Vargas(UEA) <rsv.snf17@dmz.org.br>
2021-05-20 13:24:46 -0400

    subida de teste2

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,13 @@
 console.log('process.env.DATABASE_URL :>>', process.env.DATABASE_URL)
 module.exports = {
    "type": "postgres",
+    "host": "ec2-54-145-224-156.compute-1.amazonaws.com",
+    "port": 5432,
+    "username": "iqbkxkptgwpknv",
+    "password": "6ff9946eb39fe9f2b6104049a712d97733d8c5a02ce3e8c104d197021d170098",
+    "database": "df28r7qlm981ml",
+
+
    "url":process.env.DATABASE_URL,
     "entities": [
        "dist/src/models/*.js"
