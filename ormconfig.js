35b44da9db157f0e493496675863dac2768dcaf3
Reinaldo da Silva Vargas(UEA) <rsv.snf17@dmz.org.br>
2021-05-20 17:17:41 -0400

    Novo teste

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,10 @@ module.exports = {
     "username": "nhnadhpetkevpx",
     "password": "2a2c69886377dd9fc6619ef5f08cbdd795451d4dbadba49e5c2d131165e58a2e",
     "database": "dbgoiekp2vseuo",
-   "url":process.env.DATABASE_URL,
+    "database": "dbgoiekp2vseuo",
+   "ssl": {
+      "rejectUnauthorized": false
+   },
     "entities": [
        "dist/src/models/*.js"
     ],
